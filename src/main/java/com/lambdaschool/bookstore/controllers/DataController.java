a122418dc9f39336c3f1e00c04336384d4c63746
Adam Mathieson <mathieson1222@gmail.com>
2019-08-02 16:27:58 -0700

    adds dataController, updateBook - tested

--- src/main/java/com/lambdaschool/bookstore/controllers/DataController.java
+++ src/main/java/com/lambdaschool/bookstore/controllers/DataController.java
@@ -1,10 +1,19 @@
 package com.lambdaschool.bookstore.controllers;
 
+import com.lambdaschool.bookstore.models.Book;
+import com.lambdaschool.bookstore.models.ErrorDetail;
 import com.lambdaschool.bookstore.services.AuthorService;
 import com.lambdaschool.bookstore.services.BookService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/data")
@@ -16,5 +25,16 @@ public class DataController
     @Autowired
     private AuthorService authorService;
 
-    
+    @ApiOperation(value = "Udates a books info", consumes = "Book", response = void.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Book Successfully Updated", response = void.class),
+            @ApiResponse(code = 404, message = "Book Not Found", response = ErrorDetail.class),
+            @ApiResponse(code = 500, message = "Error Updating Book", response = ErrorDetail.class),
+    })
+    @PutMapping(value = "/books/{id}", produces = {"application/json"}, consumes = {"application/json"})
+    public ResponseEntity<?> updateBook(@ApiParam(name = "Book Id", required = true) @PathVariable long id, @RequestBody @Valid Book book)
+    {
+        bookService.update(book, id);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
 }
